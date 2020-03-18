f0bbc88af8943f55f82ba652ee4dc8ac1baa4741
Stefan Brinkmann <stefan.brinkmann@posteo.de>
2020-03-18 13:31:14 +0100

    Intergrated two functions into one

--- src/HPFilterFunc.jl
+++ src/HPFilterFunc.jl
@@ -1,9 +1,9 @@
 
-#"""
-#    HPfilter(x::Vector, λ::Int)
-#
-#Apply the Hodrick-Prescott decomposition to vector x with multiplier value λ
-#"""
+"""
+    HPfilter(x::Vector, λ::Int)
+
+Apply the Hodrick-Prescott decomposition to vector x with multiplier value λ
+"""
 
 function HPfilter(x::Vector, λ::Int)
     n = length(x)
@@ -17,13 +17,13 @@ function HPfilter(x::Vector, λ::Int)
     return (I + λ * D * D') \ x
 end
 
-#"""
-#    HPfilter(x::Vector, λ::Int; iter::Int = 1)
-#
-#Compute boosted Hodrick-Prescott filter with number of iterations specified by iter
-#"""
+"""
+    HPfilter(x::Vector, λ::Int, iter::Int = 1)
 
-function HPfilter(x::Vector, λ::Int; iter::Int = 1)
+Compute boosted Hodrick-Prescott filter with number of iterations specified by iter
+"""
+
+function HPfilter(x::Vector, λ::Int, iter::Int = 1)
     n = length(x)
     m = 2
     @assert n > m
@@ -48,15 +48,18 @@ function HPfilter(x::Vector, λ::Int; iter::Int = 1)
     return x - solve(S,x,iter)
 end
 
-#"""
-#    HPfilter(x::Vector, λ::Int; Criterion="BIC", max_iter::Int = 100)
-#
-#Compute boosted Hodrick-Prescott filter with stop criterion using Bayesian-type
-#information criterion (BIC)
-#"""
 
+"""
+    bHPfilter(x::Vector, λ::Int; Criterion="BIC", max_iter::Int = 100, p::Float64=0.05)
 
-function HPfilter(x::Vector, λ::Int; Criterion="BIC", max_iter::Int = 100)
+Compute boosted Hodrick-Prescott filter with stop criterion using either a Bayesian-type
+information criterion (BIC) or an augmented Dickey-Fuller (ADF) test
+
+"""
+
+######
+
+function bHPfilter(x::Vector, λ::Int; Criterion="BIC", max_iter::Int = 100, p::Float64=0.05)
     n = length(x)
     m = 2
     @assert n > m
@@ -66,57 +69,42 @@ function HPfilter(x::Vector, λ::Int; Criterion="BIC", max_iter::Int = 100)
         -2 => fill(1, n-m) )
     @inbounds D = D[1:n,1:n-m]
     S = inv(I + λ * D * D')
-    c_j = x
-    c_p = Array{Float64, 1}()
-    c_hp = Array{Float64, 1}()
-    B = 1
-    IC = Vector{Float32}()
-    for i in 1:max_iter
-        c_p = c_j
-        c_j = (I - S) * c_j
-        if i == 1
-            c_hp = c_j
-        end
-        B *= (I - S)
-        B_j = I - B
-        push!(IC, var(c_j) / var(c_hp) + log(n) * tr(B_j) / tr(I - S))
-        if i > 1 && IC[i] > IC[i-1]
-            println("Number of iterations = $(i-1)")
-            break
-        end
-    end
-    #@printf "%s" l
-    return x - c_p
-end
 
-#"""
-#    HPfilter(x::Vector, λ::Int; Criterion="ADF", p::Float64=0.05 max_iter::Int = 100)
-#
-#Compute boosted Hodrick-Prescott filter with stop criterion using augmented Dickey-Fuller (ADF) test
-#
-#"""
-
-function HPfilter(x::Vector, λ::Int; Criterion="ADF", p::Float64=0.05, max_iter::Int = 100)
-    n = length(x)
-    m = 2
-    iter = max_iter
-    @assert n > m
-    I = Diagonal(ones(n))
-    D = spdiagm(0 => fill(1, n-m),
-        -1 => fill(-2, n-m),
-        -2 => fill(1, n-m) )
-    @inbounds D = D[1:n,1:n-m]
-    S = (I + λ * D * D')
-    function solve(S,x,iter,p)
-        f = S \ x
-        p_adf = pvalue( ADFTest(x-f, :trend, Int(floor((length(x)-1)^(1/3)))))
-        println("p-value %f \n", p_adf)
-        if iter > 1 && p_adf > p
-            return solve(S,x-f,iter-1,p)
-        else
-            println("Number of iterations: %d \n", max_iter  - iter + 1)
-            return x-f
+    if Criterion == "BIC"
+        c_j = x
+        c_p = Array{Float64, 1}()
+        c_hp = Array{Float64, 1}()
+        B = 1
+        IC = Vector{Float32}()
+        for i in 1:max_iter
+            c_p = c_j
+            c_j = (I - S) * c_j
+            if i == 1
+                c_hp = c_j
+            end
+            B *= (I - S)
+            B_j = I - B
+            push!(IC, var(c_j) / var(c_hp) + log(n) * tr(B_j) / tr(I - S))
+            if i > 1 && IC[i] > IC[i-1]
+                println("Number of iterations = $(i-1)")
+                break
+            end
         end
+        return x - c_p
     end
-    return x - solve(S,x,iter,p)
+
+    if Criterion == "ADF"
+        function solve(S,x,iter,p)
+            f = S \ x
+            p_adf = pvalue( ADFTest(x-f, :trend, Int(floor((length(x)-1)^(1/3)))))
+            println("p-value %f \n", p_adf)
+            if iter > 1 && p_adf > p
+                return solve(S,x-f,iter-1,p)
+            else
+                println("Number of iterations: %d \n", max_iter  - iter + 1)
+                return x-f
+            end
+        end
+        return x - solve(S,x,iter,p)
+    end
 end
